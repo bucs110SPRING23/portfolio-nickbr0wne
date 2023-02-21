6f3957dab964f98f06d7db4befd18720ee3c5166
Nick Browne <nbrowne2@binghamton.edu>
2023-02-20 23:57:07 -0500

    fhd

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,13 @@
+import random
 
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+classes_per_week = 10
+cost_per_week = ((tuition / classes) / weeks)
+cost_per_class = ((cost_per_week / classes_per_week))
+print("Cost per week:", cost_per_week, type(cost_per_week))
+print("Cost per class:", cost_per_class, type(cost_per_class), "  :)")
+
+#Part B
--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,99 @@
+import turtle #1
+import random
+
+import pygame
+import math
 
 #Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week)
+window = turtle.Screen() #2
+window.bgcolor('lightblue')
 
+turtle1 = turtle.Turtle() #3
+turtle2 = turtle.Turtle()
+turtle1.color('pink')
+turtle2.color('blue')
+turtle1.shape('turtle')
+turtle2.shape('turtle')
+
+turtle1.up() 
+turtle2.up()
+turtle1.goto(-100,20)
+turtle2.goto(-100,-20)
+
+range1 = random.randrange(1,100)
+range2 = random.randrange(1,100)
+print(range1, range2)
+
+turtle1.forward(range1)
+turtle2.forward(range2)
+print(range1, range2)
+
+turtle1.goto(-100,20)
+turtle2.goto(-100,-20)
+
+range3 = random.randrange(1,10)
+range4 = random.randrange(1,10)
+print(range3, range4)
+
+for i in range(0,10):
+    range3 = random.randrange(1,10)
+    range4 = random.randrange(1,10)
+    turtle1.forward(range3)
+    turtle2.forward(range4)
+
+turtle1.goto(-100,20)
+turtle2.goto(-100,20)
 
 #Part B
+import pygame
+import math
+
+pygame.init()
+window = pygame.display.set_mode()
+surface = pygame,display.get_surface()
+def shape_points(num_sides):
+    coords = []
+    num_sides = num_sides
+    side_length = 100
+    offset = 150
+
+for i in range(num_sides):
+    theta = (2.0 * math.pi * (i)) / num_sides
+    x = side_length * math.cos(theta) + offset
+    y = side_length * math.sin(theta) + offset
+    t_list = (x,y)
+    coords.append(t_list)
+    return coords
+
+pygame.display,polygon(surface, "gold", shape_points(3))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+pygame.display,polygon(surface, "green", shape_points(4))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+pygame.display,polygon(surface, "red", shape_points(6))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+pygame.display,polygon(surface, "orange", shape_points(20))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+pygame.display,polygon(surface, "purple", shape_points(100))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+pygame.display,polygon(surface, "green", shape_points(360))
+pygame.display.flip()
+pygame.time.wait(200)
+window.fill("black")
+
+turtle.done()
+window.exitonclick()
