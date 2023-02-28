0d8cc1ba5acba6a9eb830b27850361a402256a63
Nick Browne <nbrowne2@binghamton.edu>
2023-02-27 23:57:04 -0500

    lab3

---
+++ ch01/exercises/data.py
@@ -0,0 +1,13 @@
+import math
+
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10 )
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15) # the last answer is incorrect since 10/15 results in a repeating decimal
+
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,26 +1,83 @@
-import turtle #1. import modules
 import random
+import pygame
+import math
 
-#Part A
-window = turtle.Screen() # 2.  Create a screen
-window.bgcolor('lightblue')
+pygame.init()
 
-michelangelo = turtle.Turtle() # 3.  Create two turtles
-leonardo = turtle.Turtle()
-michelangelo.color('orange')
-leonardo.color('blue')
-michelangelo.shape('turtle')
-leonardo.shape('turtle')
+backcolor=("light blue")
+cicrlecolor=(0,0,200)
+hitcolor=(0,255,0)
+misscolor=(255,0,0)
+player1color=("white")
+player2color=("black")
+chosenplayer=0
+buttonsize=75
+buttondistance=10
+player1score=0
+player2score=0
 
-michelangelo.up() # 4. Pick up the pen so we don’t get lines
-leonardo.up()
-michelangelo.goto(-100,20)
-leonardo.goto(-100,-20)
+print("Who do you predict will win? (Choose White or Black)")
+window=pygame.display.set_mode()
+windowx,windowy=pygame.display.get_window_size()
+center=int(windowx)/2,int(windowy)/2
+window.fill(backcolor)
+pygame.draw.circle(window,'pink',center,center[1])
+pygame.draw.line(window, "black", (0,210), (800,210), 1)
+pygame.draw.line(window, "black", (365,0), (365,600), 1)
 
-## 5. Your PART A code goes here
 
+player1button=pygame.Rect(buttondistance,buttondistance,buttonsize,buttonsize)
+drawing1button=pygame.draw.rect(window,player1color,player1button)
+player2button=pygame.Rect(windowx-buttonsize-buttondistance,buttondistance,buttonsize,buttonsize)
+drawing2button=pygame.draw.rect(window,player2color,player2button)
+pygame.display.flip()
 
-# PART B - complete part B here
+user_has_chosen=False
 
+while not user_has_chosen:
+    for event in pygame.event.get():
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            clickposition=event.pos
+            if player1button.collidepoint(clickposition):
+                print("You predict White will win...")
+                user_has_chosen=True
+                chosenplayer=1
+            elif player2button.collidepoint(clickposition):
+                print("you predict Black will win...")
+                user_has_chosen=True
+                chosenplayer=2
 
-window.exitonclick()
+for i in range(10):
+    throwx=random.randrange(0,windowx)
+    throwy=random.randrange(0,windowy)
+    farfromcenter=math.hypot(center[0]-throwx,center[1]-throwy)
+    is_in_circle=farfromcenter<=center[1]
+    if is_in_circle:
+        dartcolor=player1color
+        player1score=player1score+1
+    else:
+        dartcolor=misscolor
+    pygame.draw.circle(window,dartcolor,(throwx,throwy),2)
+    pygame.time.wait(1000)
+    pygame.display.flip()
+
+    throwx=random.randrange(0,windowx)
+    throwy=random.randrange(0,windowy)
+    farfromcenter=math.hypot(center[0]-throwx,center[1]-throwy)
+    is_in_circle=farfromcenter<=center[1]
+    if is_in_circle:
+        dartcolor=('black')
+        player2score=player2score+1
+    else:
+        dartcolor=misscolor
+    
+    if player1score>player2score:
+        print("White wins! :D")
+    elif player2score>player1score:
+        print("Black wins! :D")
+    else:
+        print("It's a tie! :O")
+
+pygame.display.update()
+pygame.time.wait(1000)
+
