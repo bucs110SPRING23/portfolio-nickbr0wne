c2b293be216ef7482a6a2d21a5d7575ca57c6588
Nick Browne <nbrowne2@binghamton.edu>
2023-05-09 16:12:07 -0400

    lab 3 change

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -3,7 +3,7 @@ import random
 import math
 
 pygame.init()
-window=pygame.display.set_mode((800, 800))
+window=pygame.display.set_mode((600, 600))
 screen_size=window.get_size()
 center_of_screen=(screen_size[0]/2, screen_size[1]/2)
 half_width=screen_size[0]/2
@@ -12,27 +12,18 @@ width=screen_size[0]
 height=screen_size[1]
 window.fill("white")
 pygame.draw.circle(window, "black", center_of_screen, half_height) 
-pygame.draw.circle(window, "orange", center_of_screen, half_height - 5)
+pygame.draw.circle(window, "grey", center_of_screen, half_height - 5)
 pygame.draw.line(window, "black", (half_width, 0), (half_width, height), 3)
 pygame.draw.line(window, "black", (0, half_height), (width, half_height), 3)
 
-for i in range(10): 
-    dart1x=random.randrange(0, width)
-    dart1y=random.randrange(0, height)
+for i in range(10):
     dart2x=random.randrange(0, width)
     dart2y=random.randrange(0, height)
-    dart1_distance=math.hypot(dart1x-half_width, dart1y-half_height)
     dart2_distance=math.hypot(dart2x-half_width, dart2y-half_height)
-    in_circle_dart_1=(dart1_distance<=half_width)
-    in_circle_dart_2=(dart2_distance<=half_width)
-    if (in_circle_dart_1):
-        pygame.draw.circle(window, "purple", [dart1x, dart1y], 10)
-    else:
-        pygame.draw.circle(window, "pink", [dart1x, dart1y], 10)
-        
+    in_circle_dart_2=(dart2_distance<=half_width)  
     if (in_circle_dart_2):
-        pygame.draw.circle(window, "dark green", [dart2x, dart2y], 10)
+        pygame.draw.circle(window, "green", [dart2x, dart2y], 5)
     else:
-        pygame.draw.circle(window, "green", [dart2x, dart2y], 10)
+        pygame.draw.circle(window, "red", [dart2x, dart2y], 5)
 pygame.display.flip()
-pygame.time.wait(2000)
+pygame.time.wait(3000)
